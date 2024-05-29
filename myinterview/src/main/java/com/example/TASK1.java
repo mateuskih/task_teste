3a4b839197645fbe5cfdab6f40da6154cf3e71c3
Mateus <mateusoliveirdesouza@hotmail.com>
2024-05-28 22:16:48 -0300

    feat: implemented task 1 palindrome

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -1,5 +1,6 @@
 package com.example;
 import java.util.Scanner;
+
 /**
  * 
  *
@@ -13,6 +14,32 @@ import java.util.Scanner;
  * racecar.
  */
 public class TASK1 {
-     
- 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to check if it's a palindrome:");
+        String input = scanner.nextLine();
+        
+        if(isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+    
+    public static boolean isPalindrome(String str) {
+        String formattedStr = str.replaceAll("\\s+", "").toLowerCase();
+        
+        int left = 0;
+        int right = formattedStr.length() - 1;
+        
+        while(left < right) {
+            if(formattedStr.charAt(left) != formattedStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
 }
